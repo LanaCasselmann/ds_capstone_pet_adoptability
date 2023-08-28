4deb01879f866c51eac623e39ccc5edefe998162
Cazta <anna.n.stein@gmail.com>
2023-08-28 13:12:33 +0200

    random_forests default all features

--- notebooks/custom_functions.py
+++ notebooks/custom_functions.py
@@ -40,15 +40,12 @@ def our_metrics(y_true, y_pred, normalize=True):
     cm = confusion_matrix(y_true, y_pred)
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        sns.heatmap(cm, cmap="YlGnBu", annot=True);
-        print('Model Metrics and Normalized Confusion Matrix')
-        print("_____________________")
+        sns.heatmap(cm, cmap='PuOr', annot=True);
+        print('Model Metrics and Normalized Confusion Matrix:')
         print("_____________________")
     else:
-        print('Model Metrics and Confusion Matrix without Normalization')
-        print("_____________________")
-        print("_____________________")
-        sns.heatmap(cm, cmap="YlGnBu", annot=True);
+        print('Model Metrics and Confusion Matrix without Normalization:')
+        sns.heatmap(cm, cmap='PuOr', annot=True);
     print("Accuracy:", round(accuracy_score(y_true, y_pred), 4))
     print("_____________________")
     print('Weighted Quadratic Kappa:', round(cohen_kappa_score(y_true, y_pred, weights='quadratic'), 4)) 
@@ -65,3 +62,33 @@ def our_metrics(y_true, y_pred, normalize=True):
     # print('Recall', round(recall_score(y_true, y_pred), 4))
     # print("_____________________")
     # print('Specificity', round(recall_score(y_true, y_pred, pos_label=0), 4))
+
+
+# def class_metrics_var_threshold(fitted_model, X, y_true, threshold=0.5, normalize=True):
+#     """
+#     function to compute confusion matrix and classification metrics based on passed threshold.
+#     required arguments: fitted model, X_test, y_test, threshold (float, default >= 0.5)
+#     """
+#     # predicted probabilities based on fitted model
+#     proba = fitted_model.predict_proba(X)
+    
+#     # predicted y based on passed threshold
+#     y_pred = [int(i>=threshold) for i in proba[:,1]]
+
+#     # line for nicer output :)
+#     print('____________________')
+
+#     # confusion matrix of actual y and predicted y
+#     cm = confusion_matrix(y_true, y_pred)
+#     if normalize:
+#         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#         sns.heatmap(cm, cmap='PuOr', annot=True);
+#         print('Model Metrics and Normalized Confusion Matrix:')
+#         print("_____________________")
+#     else:
+#         print('Model Metrics and Confusion Matrix without Normalization:')
+#         sns.heatmap(cm, cmap='PuOr', annot=True);
+#     print("Accuracy:", round(accuracy_score(y_true, y_pred), 4))
+#     print("_____________________")
+#     print('Weighted Quadratic Kappa:', round(cohen_kappa_score(y_true, y_pred, weights='quadratic'), 4)) 
+    
